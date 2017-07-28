d32106715c0e97eb804609dc66985502b1893335
eriktks <erikt@xs4all.nl>
2017-07-28 22:39:08 +0200

    28-07-2017 22:39

--- active-select.py
+++ active-select.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3 -W all
 """
     select-active.py: select training lines for active learning
-    usage: select-active.py -d dataFile -p probFile [-c|-r|-e|-l|-m|-E] [-R] [-a]
+    usage: select-active.py -d dataFile -p probFile [-c|-r|-e|-l|-m|-E|-S] [-R] [-a] [-s simFile]
     note: command line arguments:
     -d: data file, lines correspond with those of the probabilities file
     -p: file with probabilities; line format: class1 prob1 class2 prob2 ...
@@ -11,6 +11,7 @@
     -e: select data by entropy of the best choice
     -E: select data by entropy of all classes
     -l: select longest lengths in characters
+    -S: select highest similarity
     -R: reverse selection: not the worst but the best
     -a: output all input data, first selected, then rest
     20170718 erikt(at)xs4all.nl
@@ -22,8 +23,8 @@ import random
 import sys
 
 COMMAND = sys.argv.pop(0)
-USAGE = "usage: "+COMMAND+" -p probFile -d dataFile [-c|-r|-e|-l|-m|-E] [-R] [-a]"
-SAMPLESIZE = 2
+USAGE = "usage: "+COMMAND+" -p probFile -d dataFile [-c|-r|-e|-l|-m|-E|-S] [-R] [-a] [-s simFile]"
+SAMPLESIZE = 4952
 HALFTARGET = int(float(SAMPLESIZE)/2.0)
 NBROFEXPFIELDS = 24
 dataFile = ""
@@ -35,10 +36,12 @@ useConfidence = False
 useLength = False
 useEntropyBest = False
 useEntropyAll= False
+useSimilarity= False
 outputAll = False
+simFile = ""
 data = []
 
-try: options = getopt.getopt(sys.argv,"acd:eElmp:rR",[])
+try: options = getopt.getopt(sys.argv,"acd:eElmp:rRs:S",[])
 except: sys.exit(USAGE)
 for option in options[0]:
     if option[0] == "-c": useConfidence = True
@@ -51,6 +54,8 @@ for option in options[0]:
     elif option[0] == "-m": useMargin = True
     elif option[0] == "-R": reverse = True
     elif option[0] == "-a": outputAll = True
+    elif option[0] == "-s": simFile = option[1]
+    elif option[0] == "-S": useSimilarity = True
     else: sys.exit(USAGE)
 if probFile == "" or dataFile == "": sys.exit(USAGE)
 
@@ -70,9 +75,14 @@ def selectRandom(data):
     if len(selected) < SAMPLESIZE: sys.exit(COMMAND+": selectRandom(): out of data!\n")
     return({"selected":selected,"rest":data+duplicates})
 
-def computeConfidence(fields):
+def computeConfidence(line):
     probs = {}
+    similarity = 1.0
+    fields = line["scores"].split()
     if len(fields) <= 0: sys.exit(COMMAND+": computeConfidence: empty list: fields")
+    if "similarity" in line: 
+        try: similarity = line["similarity"]
+        except: sys.exit(COMMAND+": computeConfidence: "+line["similarity"]+" is not a number\n")
     # fields format: exp1-label1 exp1-conf1 ... exp1-label12 exp1-conf12 exp2-label1
     for i in range(0,len(fields),2):
         if len(fields) < i+2: sys.exit(COMMAND+": incomplete line: "+str(fields))
@@ -81,8 +91,11 @@ def computeConfidence(fields):
     maxProb = probs[fields[0]]
     maxClass = fields[0]
     for thisClass in probs:
-        if probs[thisClass] > maxProb:
-            maxProb = probs[thisClass]
+        if not reverse and probs[thisClass]/similarity > maxProb:
+            maxProb = probs[thisClass]/similarity
+            maxClass = thisClass
+        elif reverse and probs[thisClass]*similarity > maxProb:
+            maxProb = probs[thisClass]*similarity
             maxClass = thisClass
     maxProb /= len(fields)/NBROFEXPFIELDS
     return(maxProb,maxClass)
@@ -92,10 +105,10 @@ def selectConfidence(data):
     rest = []
     duplicates = []
     seen = {}
+    counter = 0
     for line in data:
-        fields = line["scores"].split()
-        if len(fields) <= 0: sys.exit(COMMAND+": selectConfidence: missing score\n")
-        line["score"],line["class"] = computeConfidence(fields)
+        counter += 1
+        line["score"],line["class"] = computeConfidence(line)
         if len(selected) >= HALFTARGET and \
            ((not reverse and line["score"] >= selected[-1]["score"]) or \
             (reverse and line["score"] <= selected[-1]["score"])):
@@ -118,10 +131,14 @@ def selectConfidence(data):
     if len(selected) < SAMPLESIZE: sys.exit(COMMAND+": selectConfidence(): out of data!\n")
     return({"selected":selected,"rest":rest+duplicates})
 
-def computeEntropyBest(fields):
+def computeEntropyBest(line):
     total = 0
     entropy = 0.0
     classes = {}
+    similarity = 1.0
+    if "similarity" in line: similarity = line["similarity"]
+    fields = line["scores"].split()
+    if len(fields) <= 0: sys.exit(COMMAND+": computeEntropyBest: missing score\n")
     # fields format: exp1-label1 exp1-conf1 ... exp1-label12 exp1-conf12 exp2-label1
     for i in range(0,len(fields),NBROFEXPFIELDS):
         total += 1
@@ -132,7 +149,8 @@ def computeEntropyBest(fields):
     for thisClass in classes:
         p = classes[thisClass]/total
         entropy += -p*math.log(p)/math.log(2)
-    return(entropy) 
+    if not reverse: return(entropy*similarity) 
+    else: return(entropy/similarity)
 
 def selectEntropyBest(data):
     selected = []
@@ -140,9 +158,7 @@ def selectEntropyBest(data):
     duplicates = []
     seen = {}
     for line in data:
-        fields = line["scores"].split()
-        if len(fields) <= 0: sys.exit(COMMAND+": selectEntropyBest: missing score\n")
-        line["score"] = computeEntropyBest(fields)
+        line["score"] = computeEntropyBest(line)
         if len(selected) >= HALFTARGET and \
            ((not reverse and line["score"] <= selected[-1]["score"]) or
             (reverse and line["score"] >= selected[-1]["score"])):
@@ -165,10 +181,14 @@ def selectEntropyBest(data):
     if len(selected) < SAMPLESIZE: sys.exit(COMMAND+": selectEntropyBest(): out of data!\n")
     return({"selected":selected,"rest":rest+duplicates})
 
-def computeEntropyAll(fields):
+def computeEntropyAll(line):
     total = 0
     entropy = 0.0
     classes = {}
+    similarity = 1.0
+    if "similarity" in line: similarity = line["similarity"]
+    fields = line["scores"].split()
+    if len(fields) <= 0: sys.exit(COMMAND+": selectEntropyAll: missing score\n")
     # fields format: exp1-label1 exp1-conf1 ... exp1-label12 exp1-conf12 exp2-label1
     for i in range(0,len(fields),2):
         total += 1
@@ -179,7 +199,8 @@ def computeEntropyAll(fields):
     for thisClass in classes:
         p = classes[thisClass]/(total/NBROFEXPFIELDS)
         entropy += -p*math.log(p)/math.log(2)
-    return(entropy) 
+    if not reverse: return(entropy*similarity) 
+    else: return(entropy/similarity)
 
 def selectEntropyAll(data):
     selected = []
@@ -187,9 +208,7 @@ def selectEntropyAll(data):
     duplicates = []
     seen = {}
     for line in data:
-        fields = line["scores"].split()
-        if len(fields) <= 0: sys.exit(COMMAND+": selectEntropyAll: missing score\n")
-        line["score"] = computeEntropyAll(fields)
+        line["score"] = computeEntropyAll(line)
         if len(selected) >= HALFTARGET and \
            ((not reverse and line["score"] <= selected[-1]["score"]) or
             (reverse and line["score"] >= selected[-1]["score"])):
@@ -212,8 +231,12 @@ def selectEntropyAll(data):
     if len(selected) < SAMPLESIZE: sys.exit(COMMAND+": selectEntropyAll(): out of data!\n")
     return({"selected":selected,"rest":rest+duplicates})
 
-def computeMargin(fields):
+def computeMargin(line):
     classes = {}
+    similarity = 1.0
+    if "similarity" in line: similarity = line["similarity"]
+    fields = line["scores"].split()
+    if len(fields) <= 0: sys.exit(COMMAND+": selectMargin: missing score\n")
     # fields format: exp1-label1 exp1-conf1 ... exp1-label12 exp1-conf12 exp2-label1
     for i in range(0,len(fields),2):
         if i+1 >= len(fields): sys.exit(COMMAND+": computeMargin: too few elements in fields: "+str(fields))
@@ -225,7 +248,8 @@ def computeMargin(fields):
     classValues.sort()
     if len(classValues) < 2: margin = 0.0
     else: margin = classValues[-2]/classValues[-1]
-    return(margin) 
+    if not reverse: return(margin*similarity) 
+    else: return(margin/similarity)
 
 def selectMargin(data):
     selected = []
@@ -233,9 +257,7 @@ def selectMargin(data):
     duplicates = []
     seen = {}
     for line in data:
-        fields = line["scores"].split()
-        if len(fields) <= 0: sys.exit(COMMAND+": selectMargin: missing score\n")
-        line["score"] = computeMargin(fields)
+        line["score"] = computeMargin(line)
         if len(selected) >= HALFTARGET and \
            ((not reverse and line["score"] <= selected[-1]["score"]) or
             (reverse and line["score"] >= selected[-1]["score"])):
@@ -265,6 +287,9 @@ def selectLength(data):
     seen = {}
     for line in data:
         line["score"] = float(len(line["data"]))
+        if "similarity" in line:
+            if not reverse: line["score"] *= line["similarity"]
+            else: line["score"] /= line["similarity"]
         if len(selected) >= HALFTARGET and \
            ((not reverse and line["score"] <= selected[-1]["score"]) or \
             (reverse and line["score"] >= selected[-1]["score"])):
@@ -284,13 +309,48 @@ def selectLength(data):
         else: duplicates.append(rest[index])
         rest[index] = rest[-1]
         rest.pop(-1)
-    if len(selected) < SAMPLESIZE: sys.exit(COMMAND+": selectConfidence(): out of data!\n")
+    if len(selected) < SAMPLESIZE: sys.exit(COMMAND+": selectLength(): out of data!\n")
     return({"selected":selected,"rest":rest+duplicates})
+
+def selectSimilarity(data):
+    selected = []
+    rest = []
+    duplicates = []
+    seen = {}
+    for line in data:
+        if "similarity" in line: line["score"] = 1.0
+        else: line["score"] = line["similarity"]
+        if len(selected) >= HALFTARGET and \
+           ((not reverse and line["score"] <= selected[-1]["score"]) or \
+            (reverse and line["score"] >= selected[-1]["score"])):
+            rest.append(line)
+        elif not line["data"] in seen:
+            selected.append(line)
+            seen[line["data"]] = True
+            selected.sort(key=lambda item: item["score"],reverse=not reverse)
+            while len(selected) > HALFTARGET:
+                element = selected.pop(-1)
+                rest.append(element)
+    while len(selected) < SAMPLESIZE and len(rest) > 0:
+        index = int(len(rest)*random.random())
+        if not rest[index]["data"] in seen:
+            selected.append(rest[index])
+            seen[rest[index]["data"]] = True
+        else: duplicates.append(rest[index])
+        rest[index] = rest[-1]
+        rest.pop(-1)
+    if len(selected) < SAMPLESIZE: sys.exit(COMMAND+": selectSimilarity(): out of data!\n")
+    return({"selected":selected,"rest":rest+duplicates})
+
+### main
 
 try: probStream = open(probFile,"r")
 except: sys.exit(COMMAND+": cannot read file "+probFile+"\n")
 try: dataStream = open(dataFile,"r")
 except: sys.exit(COMMAND+": cannot read file "+dataFile+"\n")
+if simFile != "":
+   try: simStream = open(simFile,"r")
+   except: sys.exit(COMMAND+": cannot read file "+simFile+"\n")
 
 for probLine in probStream:
     probLine = probLine.rstrip()
@@ -298,11 +358,21 @@ for probLine in probStream:
     if dataLine == "": sys.exit(COMMAND+": too few lines in data file "+dataFile)
     dataLine = dataLine.rstrip()
     line = { "scores":probLine, "data":dataLine }
+    if simFile != "":
+       simLine = simStream.readline()
+       if simLine == "": sys.exit(COMMAND+": too few lines in similarity file "+simFile)
+       simLine = simLine.rstrip()
+       try: line["similarity"] = float(simLine)
+       except: sys.exit(COMMAND+": "+simLine+" is not a number")
     data.append(line)
 probStream.close()
 dataLine = dataStream.readline()
 dataStream.close()
 if dataLine != "": sys.exit(COMMAND+": too many lines in data file "+dataFile)
+if simFile != "":
+    simLine = simStream.readline()
+    simStream.close()
+    if simLine != "": sys.exit(COMMAND+": too many lines in sim file "+simFile)
 
 if useRandom: selectResults = selectRandom(data)
 elif useConfidence: selectResults = selectConfidence(data)
@@ -310,6 +380,7 @@ elif useEntropyBest: selectResults = selectEntropyBest(data)
 elif useEntropyAll: selectResults = selectEntropyAll(data)
 elif useLength: selectResults = selectLength(data)
 elif useMargin: selectResults = selectMargin(data)
+elif useSimilarity: selectResults = selectSimilarity(data)
 else: selectResults = selectRandom(data)
 
 for line in selectResults["selected"]:
--- similarity.py
+++ similarity.py
@@ -1,52 +1,71 @@
 #!/usr/bin/python3 -W all
 """
     similarity: compute the cosine similarities of a set of numeric vectors
-    usage: similarity < file
-    note: file contains one numeric vector per line, numbers separated by spaces
+    usage: similarity [file2] < file1
+    note: files contain one numeric vector per line, numbers separated by spaces
+          paper: Settles & Craven 2008, section 3.4, pages 1074-1075
     20170725 erikt(at)xs4all.nl
 """
 
 import math
+import numpy as np
 import sys
 
 COMMAND = sys.argv.pop(0)
 
 def computeSimilarity(vector1,vector2):
-    similarity = 0.0
-    for i in range(0,len(vector1["vector"])):
-        similarity += vector1["vector"][i]*vector2["vector"][i]
-    similarity /= vector1["length"]*vector2["length"]
+    similarity = np.inner(vector1["fields"],vector2["fields"])
+    similarity /= vector1["size"]*vector2["size"]
     return(similarity)
 
-def computeLength(data):
-    length = 0.0
-    for i in range(0,len(data)): length += data[i]*data[i]
-    length = math.sqrt(length)
-    return(length)
-
-vectors = []
-similarities = []
+vectors1 = []
+vectors2 = []
+vectors2used = False
+similarities1 = []
+similarities2 = []
 nbrOfTokens = -1
 counter = 0
+if len(sys.argv) > 0:
+    inFileName = sys.argv.pop(0)
+    try: inFileHandle = open(inFileName,"r")
+    except: sys.exit(COMMAND+": cannot read file: "+inFileName+"\n")
+    vectors2used = True
+    for line in inFileHandle:
+        line = line.rstrip()
+        fields = line.split()
+        fieldsNPstrings = np.array(fields)
+        fieldsNP = fieldsNPstrings.astype(float)
+        size = math.sqrt(np.inner(fieldsNP,fieldsNP))
+        vector = {"size":size,"fields":fieldsNP}
+        vectors2.append(vector)
+    inFileHandle.close()
 for line in sys.stdin:
     counter += 1
-    if counter == 100*int(counter/100): 
-        print(str(counter))
-        sys.stdout.flush()
+    if counter == 1000*int(counter/1000): 
+        sys.stderr.write(str(counter)+"\n")
+        sys.stderr.flush()
     line = line.rstrip()
     fields = line.split()
     if nbrOfTokens < 0: nbrOfTokens = len(fields)
     if len(fields) != nbrOfTokens: sys.exit(COMMAND+": inconsistent number of tokens per line: "+str(nbrOfTokens)+" vs "+str(len(fields)))
-    for i in range(0,len(fields)): fields[i] = float(fields[i])
-    length = computeLength(fields)
-    vector = {"length":length,"vector":fields}
-    similarity = 0.0
-    for i in range(0,len(vectors)):
-        s = computeSimilarity(vector,vectors[i])
-        similarity += s
-        similarities[i] += s
-    vectors.append(vector)
-    similarities.append(similarity)
-for i in range(0,len(similarities)): 
-    print("%0.3f" % (similarities[i]/len(vectors)))
+    fieldsNPstrings = np.array(fields)
+    fieldsNP = fieldsNPstrings.astype(float)
+    size = math.sqrt(np.inner(fieldsNP,fieldsNP))
+    vector = {"size":size,"fields":fieldsNP}
+    similarity1 = 1.0 # = computeSimilarity(vector,vector)
+    for i in range(0,len(vectors1)):
+        s = computeSimilarity(vector,vectors1[i])
+        similarity1 += s
+        similarities1[i] += s
+    vectors1.append(vector)
+    similarities1.append(similarity1)
+    if vectors2used:
+        similarity2 = 0.0
+        for i in range(0,len(vectors2)):
+            similarity2 += computeSimilarity(vector,vectors2[i])
+        similarities2.append(similarity2)
+for i in range(0,len(similarities1)): 
+    if vectors2used: similarities1[i] /= similarities2[i]
+    else: similarities1[i] /= len(vectors1)
+    print("%0.3f" % (similarities1[i]))
 
